0fc281f5433780825db7056135c1c56feefb9a23
JaydenLee1116 <vv55adss@gmail.com>
2023-05-03 15:13:27 +0900

    feat: Add header component
    - ligth/dark mode btn과 todo filter btns를 담을 컴포넌트

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,11 +1,12 @@
 import styles from '../styles/Container.module.css';
-
+import Header from './Header';
 import Item from './Item';
 
 export default function Container() {
   const tempTitles = ['6시 기상', '헬스', '강의듣기', '일찍 자기'];
   return (
     <div className={styles.container}>
+      <Header />
       {tempTitles.map((title, index) => (
         <Item title={title} key={index} />
       ))}
